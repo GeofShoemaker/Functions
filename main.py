c5c5350560826d9da9c3f038185f539239a562f0
Geof Shoemaker <geof.shoemaker@students.makeschool.com>
2020-09-22 05:01:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def area(width, height):
+    result = width * height
+    return result  
+result = area(5, 6)
+print(result)
+result = area(10,13)
+result=(4,4)
+def subtract(num1,num2):
+  result=num1-num2
+  return result
+print(subtract(6,7))
+print(subtract(13,4))
+def add(num1,num2):
+  return num1+num2
+print(add(14,12))
+print(add(17,44))
